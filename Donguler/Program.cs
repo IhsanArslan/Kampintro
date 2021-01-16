4ce4ad0808e03ca3fc9d27b82fa81c0b56755f66
wapeerxyt@gmail.com <wapeerxyt@gmail.com>
2021-01-16 21:50:45 +0100

    Class'lar ve Metod'lar

---
+++ Donguler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazilim Gelistirici Yetistirme Kampi";
+            string kurs2 = "Proglamlamaya baslangic icin temel kurs";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazilim Gelistirici Yetistirme Kampi",
+                "Proglamlamaya baslangic icin temel kurs", "Java"};
+
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ Kampintro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenligi
+            //Do not repeat your self - Kendini tekrarlama
+            //deger tutucu, alias
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalis Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artis Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi Butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanici Ayarlari Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris Yap Butonu");
+            }
+            
+
+            Console.WriteLine(kategoriEtiketi);
+           
+        }
+    }
+}
